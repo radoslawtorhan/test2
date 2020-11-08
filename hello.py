1ba85da20b762fcdfc59ead62bca446a6037583f
Radoslaw Torhan <radoslaw.torhan@gmail.com>
2020-11-08 21:15:06 +0000

    Modified file hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 print("Hello") 
 #changes in master branch to be merged with branch1
 print("Added some new stuff")
+
+from .person import Person
+p = Person("Radek", 40)
+print(p.name)
+print(p.age)
